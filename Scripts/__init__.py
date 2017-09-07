e641b058bc0cc2cdf737558953dbfe422be9cbae
Ethan Snyder <techrules08041@yahoo.com>
2017-09-06 22:34:54 -0500

    Started to add python scripts. Added __init__.py

---
+++ Scripts/__init__.py
@@ -0,0 +1,31 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+bl_info =
+{
+	"name": "Lego Importer",
+	"author": "Ethan Snyder",
+	"version": (0, 0, 1),
+	"blender": (2, 79, 0),
+	"location": "File > Import-Export",
+	"description": "Import Lego models from either LDD (.lxf) or LDraw (.ldr) files.",
+	"warning": "This plugin is still being written, expect bugs galore.",
+	"wiki_url": "https://github.com/techdude1996/Lego-Importer",
+	"support": 'TESTING',
+	"category": "Import-Export"
+}
