1fbc8a8e309f6e2c5e6c08434f066ea757175df2
Ethan Snyder <techrules08041@yahoo.com>
2017-09-06 23:57:00 -0500

    Working on import menu.

--- Scripts/__init__.py
+++ Scripts/__init__.py
@@ -23,9 +23,63 @@ bl_info =
 	"version": (0, 0, 1),
 	"blender": (2, 79, 0),
 	"location": "File > Import-Export",
-	"description": "Import Lego models from either LDD (.lxf) or LDraw (.ldr) files.",
+	"description": "Import Lego models from either LDD (.lxf) or LDraw (.ldr/.dat) files.",
 	"warning": "This plugin is still being written, expect bugs galore.",
 	"wiki_url": "https://github.com/techdude1996/Lego-Importer",
 	"support": 'TESTING',
 	"category": "Import-Export"
 }
+
+import bpy
+from bpy.props import(
+		BoolProperty,
+		FloatProperty,
+		StringProperty,
+		EmunProperty,)
+
+class ImportLego(bpy.types.Operator):
+	"""Load a Lego file (LDD/LDraw)"""
+	bl_idname = "import.lego"
+	bl_label = "Import Lego"
+	bl_space_type = "PROPERTIES"
+	bl_region_type = "WINDOW"
+	bl_options ={'PRESET', 'UNDO'}
+
+	filename_ext = ".ldr"
+	filer_glob = StringProperty(default="*.lxf;*.ldr;*.dat",options={'HIDDEN'})
+
+	resolution = EnumProperty(
+		name="Resolution of parts",
+		description="Resolution of parts",
+		default=prefs.get("resolution", "LowRes"),
+		items=(
+			("LowRes", "Low Resolution", "Import using low resolution parts. Great for far distance objects."),
+			("HighRes", "High Resolution", "Import using high resolution parts. Great for close ups.")
+		)
+	)
+
+	include_type = EnumProperty(
+		name="Import type:",
+		description="Link or Append the Lego parts into the current scene",
+		default=prefs.get("include_type", "link"),
+		items=(
+			("link", "Link", "Link the parts from the library; updates scene when library changes."),
+			("append", "Append", "Append the parts from the library; creates a copy of the library.")
+		)
+	)
+
+	create_materials = BoolProperty(
+		name="Create Materials",
+		description="Create Cycles Materials for the Lego bricks using Principled BSDF.",
+		default=True;
+	)
+
+def menu_func(self, context):
+	self.layout.operator(ImportLego.bl_idname, text="Lego (LDD/LDraw)")
+
+def register():
+	bpy.utils.register_module(__name__)
+	bpy.types.INFO_MT_file_import.append(menuImport)
+
+if __name__ == "__main__":
+	register()
